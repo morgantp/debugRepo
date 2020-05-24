944939e1c92a01eaa0417e115983feed1bf32459
Daniel Smith <danielsmithwba@daniels-air-9.home>
2020-05-24 19:32:34 +0100

    general changes

--- public/js/script.js
+++ public/js/script.js
@@ -27,3 +27,19 @@ if(queryString == '?incorrectLogin' ) {
 if(queryString == '?contactSaved' ) {
     messageContainer.innerHTML = `<div class ="card panel green">Contact Saved</div>`; //doesnt work
 }
+
+const navBrand = document.querySelector('.navbar-brand');
+navBrand.addEventListener('mouseenter', addBounce);
+navBrand.addEventListener('animationend', removeBounce);
+
+const navLinks = document.querySelectorAll('.nav-link');
+navLinks.forEach(navLink => navLink.addEventListener('mouseenter', addBounce));
+navLinks.forEach(navLink => navLink.addEventListener('animationend', removeBounce));
+
+function addBounce(e){
+  this.classList.add('animated', 'bounce');
+}
+
+function removeBounce(e){
+  this.classList.remove('animated', 'bounce');
+}
